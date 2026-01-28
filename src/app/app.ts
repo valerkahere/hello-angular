29103f6636558b71c7404061a0dad2b0cdbc3a4a
Valerii Navalnyi <adultvalerka@gmail.com>
2026-01-28 11:04:26 +0000

    Test signal

--- src/app/app.ts
+++ src/app/app.ts
@@ -9,4 +9,7 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('hello-world');
+  constructor() {
+    this.title.set('hello-world-new-title');
+  }
 }
