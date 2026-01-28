48b41e8a978a4f14d8ca1e00f13b691a580d58dd
Valerii Navalnyi <adultvalerka@gmail.com>
2026-01-28 11:22:19 +0000

    Event binding - listeners

--- src/app/app.ts
+++ src/app/app.ts
@@ -7,13 +7,22 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('hello-world');
+export class App { // app class defined how this component behaves
+  // and the logic via methods and props associated with it
+
+  protected readonly title = signal('hello-world'); // protected means can only be used by this class?
+  // readonly prevents from being overwritten
   protected messageColour = signal('green');
   protected messageID = signal('StudentMessage-01');
   protected message = signal('Hello student from Lab02');
 
   constructor() {
     this.title.set('hello-world-new-title');
+  }
+
+  protected alertMessage = signal("This is an alert");
+
+  protected sendAlertMessage() {
+    alert(this.alertMessage);
   }
 }
